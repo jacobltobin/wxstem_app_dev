bb2114a94473406dba5e8578ee124e7967bacc69
Jake Tobin <jacobltobin@gmail.com>
2018-11-15 14:36:49 -0500

    local form validation logic

--- src/views/LogIn/LogIn.js
+++ src/views/LogIn/LogIn.js
@@ -23,13 +23,20 @@ class LogInView extends Component {
   static propTypes = {
     navigation: PropTypes.object,
     login_user: PropTypes.func,
+    login_info: PropTypes.object,
   }
 
   constructor(props) {
     super(props)
     this.state = {
-      uid_input: null,
-      pwd_input: null,
+      uid_input: '',
+      pwd_input: '',
+      uid_error: true,
+      uid_error_message: '',
+      uid_error_from_blur: false,
+      pwd_error: true,
+      pwd_error_message: '',
+      pwd_error_from_blur: false,
     }
   }
 
@@ -42,14 +49,65 @@ class LogInView extends Component {
     this.setState({ uid_input: text })
   }
   pwd_input_change = text => {
-    this.setState({ pwd_input: text })
+    if (this.state.pwd_error_from_blur) {
+      this.setState({
+        pwd_input: text,
+        pwd_error: text.length > 5 ? false : true,
+        pwd_error_message:
+          text.length > 5 ? '' : 'password should be longer than 5 characters',
+      })
+    } else {
+      this.setState({
+        pwd_input: text,
+        pwd_error: text.length > 5 ? false : true,
+      })
+    }
   }
 
+  validate = (field, text) => {
+    switch (field) {
+      case 'uid':
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(text)) {
+          this.setState({
+            uid_error: false,
+            uid_error_message: '',
+          })
+        } else {
+          this.setState({
+            uid_error: true,
+            uid_error_message: 'invalid email address',
+          })
+        }
+        break
+      case 'pwd':
+        if (text.length < 6) {
+          this.setState({
+            pwd_error: true,
+            pwd_error_message: 'password should be longer than 5 characters',
+            pwd_error_from_blur: true,
+          })
+        } else {
+          this.setState({
+            pwd_error: false,
+            pwd_error_message: '',
+            pwd_error_from_blur: false,
+          })
+        }
+      // todo ~~
+    }
+  }
+
+  setError = (field, message) => {}
+
   login_user = () => {
-    this.props.login_user({
-      uid: this.state.uid_input,
-      pwd: this.state.pwd_input,
-    })
+    if (!this.state.uid_error && !this.state.pwd_error) {
+      this.props.login_user({
+        uid: this.state.uid_input,
+        pwd: this.state.pwd_input,
+      })
+    } else {
+      // UI something ?
+    }
   }
 
   render() {
@@ -80,6 +138,11 @@ class LogInView extends Component {
                 containerStyle={styles.login_containerStyle}
                 inputContainerStyle={styles.login_inputContainerStyle}
                 placeholder="email"
+                onBlur={() => {
+                  this.validate('uid', this.state.uid_input)
+                }}
+                errorStyle={{ color: 'red' }}
+                errorMessage={this.state.uid_error_message}
                 autoCapitalize={'none'}
                 onChangeText={text => this.uid_input_change(text)}
                 leftIcon={{ type: 'font-awesome', name: 'user', color: '#ccc' }}
@@ -91,11 +154,17 @@ class LogInView extends Component {
                 secureTextEntry
                 onChangeText={text => this.pwd_input_change(text)}
                 placeholder="password"
+                onBlur={() => {
+                  this.validate('pwd', this.state.pwd_input)
+                }}
+                errorStyle={{ color: 'red' }}
+                errorMessage={this.state.pwd_error_message}
                 leftIcon={{ type: 'font-awesome', name: 'lock', color: '#ccc' }}
               />
               <Button
                 title="Sign In"
                 onPress={() => this.login_user()}
+                disabled={this.state.uid_error || this.state.pwd_error}
                 buttonStyle={styles.login_buttonStyle}
               />
             </View>
@@ -112,7 +181,9 @@ class LogInView extends Component {
 }
 
 const mapStateToProps = state => {
-  return {}
+  return {
+    login_info: APISelectors.selectLoginInfo(state),
+  }
 }
 
 const mapDispatchToProps = dispatch => {
